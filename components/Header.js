702cb548321aacc138c1cae42162260387836fb4
Busdy Token BSC <106945700+busdybsc@users.noreply.github.com>
2022-10-15 14:14:55 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -77,7 +77,7 @@ const Header = (props) => {
                                         />
                                     </div>
                               </div>
-                                <!--div className={styles.header__logo_img}>
+                                <div className={styles.header__logo_img}>
                                     <div className={styles.header__logo_inner} style={headerStyle}>
                                     <Image
                                             styles={{}}
@@ -87,7 +87,7 @@ const Header = (props) => {
                                             height={199}
                                         />
                                     </div>
-                                </div--!>
+                                </div>
                                 </>
                             </Link>
                         </div>
