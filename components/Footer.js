a7e4cf608090a8937425ab3327bcde7d47db354f
Busdy Token BSC <106945700+busdybsc@users.noreply.github.com>
2022-10-15 13:22:31 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -19,8 +19,8 @@ const Footer = () => {
                     styles={{ marginRight: "3rem" }}
                     src="/images/TT_silver3blackedge.png"
                     alt="Blockblend"
-                    width={140}
-                    height={87}
+                    width={180}
+                    height={110}
                     layout="fixed"
                   />
                 </a>
