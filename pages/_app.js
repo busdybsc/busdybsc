3b62a389ff7660a808dd6087f2ce1936fa86d229
Busdy Token BSC <106945700+busdybsc@users.noreply.github.com>
2022-10-17 19:51:31 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -20,8 +20,8 @@ function MyApp({ Component, pageProps }) {
         <meta property="twitter:description" content="Aynonymous cross-chain transactions. Anonymous CRO, Polygon, Matic, ETH, BNB, CRO, Fantom bridge. - $BUSDY" />
         <meta property="twitter:image" content="/images/social.jpg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/Tiddy Twister circle 32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/Tiddy Twister circle 16x16.png" />
         <link rel="manifest" href="/images/site.webmanifest" />
         <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#E14D81" />
         <meta name="msapplication-TileColor" content="#E14D81" />
