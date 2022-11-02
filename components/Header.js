d533a72429d6ae30920ab18ba7a10992ac60bbe6
Busdy Token BSC <106945700+busdybsc@users.noreply.github.com>
2022-11-02 18:13:06 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -65,7 +65,7 @@ const Header = (props) => {
                         <div className={styles.header__logo} onClick={() => window.location = 'https://www.tiddytwister.com'}>
                             <Link href="https://www.tiddytwister.com">
                                 <>
-                                <div className={styles.header__logo_img} style={{ width: !isMobile ? '90px' : 'auto', height: '90px'}}>
+//                                <div className={styles.header__logo_img} style={{ width: !isMobile ? '90px' : 'auto', height: '90px'}}>
 //                                     <div className={styles.header__logo_inner}>
 //                                         <Image
 //                                             styles={{}}
